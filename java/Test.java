54fc33a6236c31e3dbb020b76b2021783a65927b
javascript-lee <53223620+javascript-lee@users.noreply.github.com>
2019-08-22 13:17:23 +0000

    Initial commit

---
+++ java/Test.java
@@ -0,0 +1,6 @@
+public class Test{
+	public static void main (String[]args){
+		System.out.print("ÎÒ°®Ñ§Ï°\n");
+			System.out.print("ÎÒÒª±¬È«³¡");
+	}
+}
---
+++ java/Test6.java
@@ -0,0 +1,12 @@
+public class Test6{
+	public static void main(String[]args){
+		int A=20;
+		int B=40;
+		int C=A;
+		A=B;
+		B=C;
+		System.out.println("AµÄÖµÎª"+A);
+		System.out.println("BµÄÖµÎª"+B);
+	}
+
+}
